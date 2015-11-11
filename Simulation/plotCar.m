fd1cd85115a30e84fea66a2e983be10145a6f34f
Daniel Pihlquist <daniel.pihlquist@gmail.com>
2015-11-11 17:38:55 +0100

    Simulation sortof works

---
+++ Simulation/plotCar.m
@@ -0,0 +1,29 @@
+function plotCar(x, y, phi, delta)
+
+
+car = [-1.5, 1.5, 1.5, -1.5, -1.5;
+       -1,   -1,  1,    1    -1];
+ct = cos(phi);
+st = sin(phi);
+
+R = [ct, -st; st, ct];
+
+rot = R*car;
+
+td = phi+delta;
+
+% ct = cos(-phi);
+% st = sin(-phi);
+
+xtheta = 3*ct;
+ytheta = 3*st;
+
+xtd = 3*cos(td);
+ytd = 3*sin(td);
+
+plot(x + rot(1, :), y+rot(2, :));
+plot([x, x+xtheta], [y, y+ytheta], 'r');
+plot([x, x+xtd], [y, y+ytd], 'b');
+
+
+end
