445cc979f1da04d19e91df348a0acf8adc45e993
Christoffersk <Christoffersk@gmail.com>
2015-11-11 13:05:27 +0100

    Constraints

--- Test MPC/plotcar.m
+++ Test MPC/plotcar.m
@@ -1,8 +1,8 @@
-function []=plotcar(pos1,pos2)
+function []=plotcar(pos1,pos2,colour)
 
 % plot the road
 figure(1)
 hold on
-plot(pos1,pos2,'r*')
+plot(pos1,pos2,colour)
 %rectangle('Position', [pos1 pos2-task.road.lanewidth/4 20 task.road.lanewidth/2])
 end
