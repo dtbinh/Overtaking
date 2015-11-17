b31118a25769f5e9d93947ef84aa2cdabe28154b
Karamba91 <erikhenriksson91@gmail.com>
2015-11-17 16:57:16 +0100

    2015-11-17

--- Test MPC/Moving_reference_frame.m
+++ Test MPC/Moving_reference_frame.m
@@ -1,4 +1,5 @@
 %% Moving reference frame
+clear;clc;clf;close;
 ds=1;                                   % delta s = 1 meter
 A = [0 0;0 0];
 B = [1 0;0 1];
@@ -12,19 +13,19 @@ D = [0 0;0 0];
 % H = [0 1];                              % Selection matrix
 % Cz= H*C;
 
-ph = 150;                                 % Prediction horizon
+ph = 50;                                 % Prediction horizon
 
 laneWidth = 2.5;                        % Lanewidth
 ysp=[ones(1,ph)*(5);ones(1,ph)*2.5];    % Setpoint for xdot and y pos.
 x0=[2;4];                     % Initial state value
 
 % State/input penalty
-q = 2;
+q = 4;
 r = 1;
 
-H = blkdiag(kron(eye(ph),C'*q*C),kron(eye(ph),eye(m)*r));
+H = 2*blkdiag(kron(eye(ph),C'*q*C),kron(eye(ph),eye(m)*r));  % Extend costfunction with, same things as nikolce.
 
-f1 = 2*(C*ysp)';f=[];
+f1 = 2*(C*ysp)'*q;f=[];
 for i=1:size(f1,1)
     f = [f;f1(i,1);f1(i,2)];
 end
@@ -35,11 +36,12 @@ Aeq =[[zeros(2,n*ph);-eye((ph-1)*n) zeros((ph-1)*n,2)]+eye(ph*n) kron(eye(ph),-B
 beq = zeros(size(Aeq,1),1);beq(1:2)=x0;
 
 % Inequality constraints
-ymin=ones(1,ph)*eps;ymin(100:120)=4;
+ymin=ones(1,ph)*eps;
+ymin(floor(ph/4*2):floor(ph/4*3))=4;
 ymax=ones(1,ph)*laneWidth*2;
 
-Ain=zeros(1,(m+n)*ph);Ain(n*ph+2)=-1;Ain(2,n*ph+4)=-1;
-bin=[0;0];
+Ain=[-kron(eye(ph),[0 1]) zeros(ph,ph*m)];
+bin=-ymin';
 
-z=quadprog(H,f,[],[],Aeq,beq);
+z=quadprog(H,f,Ain,bin,Aeq,beq,[],[],x0);
 
